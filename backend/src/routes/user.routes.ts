a52bc2ff75ff02aa6c7386d71964f9477ec03062
Hardik-Kumar1912 <hardikk1401@gmail.com>
2025-07-07 00:07:12 +0530

    Removed backend folder and cleaned repo for deployed backend

--- backend/src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express";
-import {updateProfileController} from "../controllers/user/updateProfileController.js";
-
-const router = express.Router();
-
-router.post("/profile", updateProfileController);
-
-export default router;
